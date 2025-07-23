774f0124824f22a1bb704a8b679fe9ce04545d52
Shadi Ibrahim <shadi.rbis@gmail.com>
2025-07-23 22:51:51 +0300

    Worked on Capsule model.

--- app/Models/Capsule.php
+++ app/Models/Capsule.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Capsule extends Model
 {
-    //
+use HasFactory;
+
+/* protected $fillable = [
+    'user_id', 'title', 'message', 'mood',
+    'reveal_at', 'is_public', 'ip_address',
+    'latitude', 'longitude'
+];
+*/
+
+public function user(){
+    return $this->belongsTo(User::class, 'user_id');
+}
+
 }
