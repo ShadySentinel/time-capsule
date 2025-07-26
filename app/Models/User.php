d7b38fe018b045c90a31ea14c7b8b0efadb23a85
Shadi Ibrahim <shadi.rbis@gmail.com>
2025-07-26 20:58:32 +0300

    Worked on User model.

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,47 +2,53 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    protected $fillable = ['name', 'email', 'password', 'is_admin'];
+    protected $hidden = ['password', 'remember_token'];
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
+    public function capsules()
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        return $this->hasMany(Capsule::class);
+    }
+
+    public static function create(array $data)
+    {
+        try {
+            $user = new self();
+            $user->fill([
+                'name' => $data['name'],
+                'email' => $data['email'],
+                'password' => bcrypt($data['password']),
+                'is_admin' => $data['is_admin'] ?? false,
+            ]);
+            $user->save();
+            return true;
+        } catch (\Exception $e) {
+            throw new \Exception("Error creating user: " . $e->getMessage());
+        }
+    }
+
+    public static function findAll()
+    {
+        try {
+            return self::all();
+        } catch (\Exception $e) {
+            throw new \Exception("Error fetching users: " . $e->getMessage());
+        }
+    }
+
+    public static function find($id)
+    {
+        try {
+            return self::findOrFail($id);
+        } catch (\Exception $e) {
+            throw new \Exception("Error fetching user: " . $e->getMessage());
+        }
     }
 }
