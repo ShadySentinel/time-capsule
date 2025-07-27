110b69f73fcb9abb86a6d28bad2c6e9a5ff8414a
Shadi Ibrahim <shadi.rbis@gmail.com>
2025-07-27 22:34:24 +0300

    Modified routes for testing 2 APIs.

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CapsuleController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/capsules', [CapsuleController::class, 'addCapsule']);
+
+Route::get('/capsules/public', [CapsuleController::class, 'getAllPublicCapsules']);
